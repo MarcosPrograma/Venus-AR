3b115e822ff473d6ac478985da8076e607e5d2cd
Marcos Juárez Agüero <MarcosEJA01@gmail.com>
2026-05-10 18:04:05 -0300

    cargarVenus + estilo

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 const modelViewer = document.querySelector('#visor-ar');
 
 const modelos = {
-    'efebo': {
+    'venus_1': {
         src: 'assets/venus_1.glb',
         alt: 'Venus de Capua en estado original',
-        //poster: 'assets/poster-efebo.webp'
+        poster: 'assets/poster-venus_1.webp'
     },
+        'venus_2': {
+        src: 'assets/venus_2.glb',
+        alt: 'Venus de Capua restaurada',
+        poster: 'assets/poster-venus_2.webp'
+    },
+        'venus_3': {
+        src: 'assets/venus_3.glb',
+        alt: 'Venus de Capua y la Manzana de la Discordia',
+        poster: 'assets/poster-venus_3.webp'
+    },
+        'venus_4': {
+        src: 'assets/venus_4.glb',
+        alt: 'Venus de Capua (Afrodita), diosa del amor y su relación simbólica con Marte (Ares), dios de la guerra. Amor y Guerra',
+        poster: 'assets/poster-venus_4.webp'
+    },
+        'venus_5': {
+        src: 'assets/venus_5.glb',
+        alt: 'Venus de Capua y vínculo con el arte, la música y la belleza',
+        poster: 'assets/poster-venus_5.webp'
+    },
+};
+
+window.switchModel = (element, name) => {
+    const modelo = modelos[name];
+
+    modelViewer.src = modelo.src;
+    modelViewer.alt = modelo.alt;
+    modelViewer.poster = modelo.poster;
+
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((el) => el.classList.remove("selected"));
+    element.classList.add("selected");
 };
 
 function switchScreen(screen) {
